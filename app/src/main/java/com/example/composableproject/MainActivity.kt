27a335490344c07ca17ddace22524002be2310a7
DESKTOP-4NOP5MA\Vince <johnvincentdallego082193@gmail.com>
2024-04-20 11:15:36 +0800

    composable  basic

--- app/src/main/java/com/example/composableproject/MainActivity.kt
+++ app/src/main/java/com/example/composableproject/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.composableproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composableproject.ui.theme.ComposableProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +29,77 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposableProjectTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(
+                        message = "Happy Birthday Fat!",
+                        from = "from Vince",
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    ComposableProjectTheme {
-        Greeting("Android")
+fun GreetingImage(message: String, from : String,  modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_background)
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+
     }
 }
+
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+
+    }
+
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ComposableProjectTheme {
+//        GreetingText(
+//            message = "Happy Birthday Fat!",
+//            from = "vince",
+//            modifier = Modifier.padding(8.dp)
+//        )
+//    }
+//}
